ec2d88495061b3eedeafb1e6af7130ad74e9d7ce
vishalchopra <vishchopra33@gmail.com>
2016-03-02 23:30:43 +0530

    unwanted file

--- modules/.init_sc.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vishal/Kernel_world/DEVICE_DRIVER_SCRACTH/init_sc.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/vishal/Kernel_world/DEVICE_DRIVER_SCRACTH/init_sc.ko /home/vishal/Kernel_world/DEVICE_DRIVER_SCRACTH/init_sc.o /home/vishal/Kernel_world/DEVICE_DRIVER_SCRACTH/init_sc.mod.o
